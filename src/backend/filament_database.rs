cba5b41bbea340a84f5589331f1c6df877a5befc
bliatun <bliatun@users.noreply.github.com>
2026-05-16 02:52:39 +0200

    Move loan models

--- src/backend/filament_database.rs
+++ src/backend/filament_database.rs
@@ -54,6 +54,9 @@ use super::database_loan_create::{
     create_inbound_spool_loan as create_inbound_spool_loan_row,
     create_spool_loan as create_spool_loan_row,
 };
+pub use super::database_loan_models::{
+    ActiveSpoolLoanRow, LoanUsageByPersonRow, SpoolLoanDetailsRow, SpoolLoanRow,
+};
 use super::database_loan_queries::{
     find_active_spool_loan_for_direction as find_active_spool_loan_for_direction_row,
     list_active_spool_loans as list_active_spool_loan_rows,
@@ -175,60 +178,6 @@ pub struct CatalogResetStats {
     pub removed_count: i64,
     pub remaining_count: i64,
     pub reactivated_count: i64,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct SpoolLoanRow {
-    pub id: String,
-    pub spool_id: String,
-    pub borrower_name: String,
-    pub loan_direction: String,
-    pub loan_status: String,
-    pub counterparty_name: String,
-    pub counterparty_contact: Option<String>,
-    pub counterparty_note: Option<String>,
-    pub grams_out: i64,
-    pub lent_note: Option<String>,
-    pub lent_at: String,
-    pub expected_return_at: Option<String>,
-    pub returned_at: Option<String>,
-    pub returned_grams: Option<i64>,
-    pub consumed_grams: Option<i64>,
-    pub return_note: Option<String>,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct ActiveSpoolLoanRow {
-    pub loan: SpoolLoanRow,
-    pub spool_status: String,
-    pub spool_remaining_g: Option<i64>,
-    pub material: String,
-    pub filament_name: String,
-    pub color_name: String,
-    pub vendor: String,
-    pub hex_color: Option<String>,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct LoanUsageByPersonRow {
-    pub loan_direction: String,
-    pub borrower_name: String,
-    pub total_consumed_g: i64,
-    pub completed_loans: i64,
-    pub active_loans: i64,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct SpoolLoanDetailsRow {
-    pub loan: SpoolLoanRow,
-    pub spool_status: Option<String>,
-    pub spool_remaining_g: Option<i64>,
-    pub spool_tare_weight_g: Option<i64>,
-    pub material: Option<String>,
-    pub filament_name: Option<String>,
-    pub color_name: Option<String>,
-    pub vendor: Option<String>,
-    pub hex_color: Option<String>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
